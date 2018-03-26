1e19f590f197e7fc1aecaeb47332a7e79f8c86ed
Joey Kimsey <Joeyk4816@gmail.com>
2018-03-26 19:16:01 -0400

    contact form tracking
    added tracking to comment form

--- Views/Admin/contact.php
+++ Views/Admin/contact.php
@@ -33,6 +33,12 @@
             <textarea class="form-control" name="mailMessage" maxlength="2000" rows="10" cols="50" id="mailMessage"></textarea>
         </div>
     </div>
+    <div class="form-group">
+        <label for="trackOpens" class="col-lg-3 control-label">Track opens</label>
+        <div class="col-lg-3">
+            <input type="checkbox" name="trackOpens" id="trackOpens">
+        </div>
+    </div>
     </fieldset>
     <input type="hidden" name="token" value="{TOKEN}">
     <button name="submit" value="submit" type="submit" class="btn btn-lg btn-primary center-block">Send</button><br>
